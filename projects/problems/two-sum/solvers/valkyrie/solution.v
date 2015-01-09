b3c5a294c514e469e3cbfa8b8a524c427a192367
aster <aster@vers.site>
2015-01-09 13:07:36 +0000

    📊 Refresh batch-20 benchmark snapshots after Python conformance fixes

--- projects/problems/two-sum/solvers/valkyrie/solution.v
+++ projects/problems/two-sum/solvers/valkyrie/solution.v
@@ -1,8 +1,6 @@
 namespace leetcode.two_sum;
 
-# wasm invoke 入口：V 侧 snake_case，导出符号 camelCase 对齐 metadata.invoke
-[export(case: "camelCase")]
-micro two_sum(nums: ArrayList<i64>, target: i64): Option<ArrayList<i64>> {
+micro two_sum(nums: ArrayList<i64>, target: i64) -> ArrayList<i64> {
     let mut index: HashMap<i64, i64> = HashMap::new(0)
     let mut i: usize = 0
     while i < nums.length() {
@@ -12,10 +10,10 @@ micro two_sum(nums: ArrayList<i64>, target: i64): Option<ArrayList<i64>> {
             let mut result: ArrayList<i64> = ArrayList::new(2)
             result.push(index.get(need).unwrap())
             result.push(i as i64)
-            return Some(result)
+            return result
         }
         index.insert(x, i as i64)
         i = i + 1
     }
-    return None
+    return ArrayList::new(0)
 }
